24c4332f194c1c7905a343c2d5d1d0a0a1928bcd
jimjones26 <jimjones26@gmail.com>
2017-02-28 17:42:41 -0500

    scaffold thread list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { UserSelectionComponent } from './user-selection/user-selection.component';
 import { ThreadSectionComponent } from './thread-section/thread-section.component';
 import { MessageSectionComponent } from './message-section/message-section.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserSelectionComponent,
     ThreadSectionComponent,
-    MessageSectionComponent
+    MessageSectionComponent,
+    ThreadListComponent
   ],
   imports: [
     BrowserModule,
