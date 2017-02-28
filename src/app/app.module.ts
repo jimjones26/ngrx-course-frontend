12797cb9cbbd40f945a2e0568a4761976f72b768
jimjones26 <jimjones26@gmail.com>
2017-02-28 17:03:52 -0500

    scaffold user selection component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSelectionComponent
   ],
   imports: [
     BrowserModule,
