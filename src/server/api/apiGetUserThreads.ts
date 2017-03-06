81a78e28940af8da35fb8198b2d7959dc42962c8
jimjones26 <jimjones26@gmail.com>
2017-03-05 21:06:50 -0500

    begin api server

---
+++ src/server/api/apiGetUserThreads.ts
@@ -0,0 +1,35 @@
+import { Application, Request, Response } from 'express';
+import { findDbThreadsPerUser } from '../persistence/findDbThreadsPerUser';
+import { dbMessages, dbParticipants } from '../db-data';
+import * as _ from 'lodash';
+import { Message } from '../../../_shared/model/message';
+import { AllUserData } from "../../../_shared/transfer-objects/all-user-data";
+
+// gets all threads for a specific user
+// gets all messages for each thread
+// gets all users participating in each thread
+export function apiGetUserThreads(app: Application) {
+  app.route('/api/threads').get((req: Request, res: Response) => {
+
+    const participantId = 1;
+    const threadsPerUser = findDbThreadsPerUser(participantId);
+    let messages: Message[] = [],
+      participantIds: string[] = [];
+
+    threadsPerUser.forEach(thread => {
+      const threadMessages: Message[] = _.filter(dbMessages, (message: any) => message.threadId = thread.id);
+      messages.concat(threadMessages);
+      participantIds.concat(_.keys(thread.participants));
+    });
+
+    const participants = _.uniq(participantIds.map(participantId => dbParticipants[participantId]));
+
+    const response: AllUserData = {
+      participants,
+      messages,
+      threads: threadsPerUser
+    };
+
+    res.status(200).json(response);
+  });
+}
